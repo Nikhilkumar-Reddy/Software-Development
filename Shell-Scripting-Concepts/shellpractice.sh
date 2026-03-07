75c2fce1a1f96c8a7b30fc1b0b92f6a6638a263d
Nikhil <nirhalnihar8344@gmail.com>
2026-03-08 02:14:42 +0530

    latest update

--- Shell-Scripting-Concepts/shellpractice.sh
+++ Shell-Scripting-Concepts/shellpractice.sh
@@ -27,7 +27,7 @@ echo " Which server do you want to uninstall? :"
 read server_uninstall
 read -p "Enter the 'yes' to uninstall or 'no' to keep: " uninstall_choice
 
-if [ "$server_uninstall" == "$uninstall_choice" ]; then
+if [ "$uninstall_choice" == "yes" ]; then
     dnf remove $server_uninstall -y
     if [ $? -eq 0 ]; then   
         echo "$server_uninstall is uninstalled successfully!"
