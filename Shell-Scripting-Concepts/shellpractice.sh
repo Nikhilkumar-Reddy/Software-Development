438a44269365679452b66e0c735f023bbe79fe98
Nikhil <nirhalnihar8344@gmail.com>
2026-03-08 00:52:29 +0530

    latest update

--- Shell-Scripting-Concepts/shellpractice.sh
+++ Shell-Scripting-Concepts/shellpractice.sh
@@ -6,8 +6,7 @@ if [ $userid -eq 0 ]; then
     echo "You are running this script as root."
     echo "You have root privileges."
     echo " Install Nginx Web Server "
-    apt update
-    apt install nginx -y
+    dnf install nginx -y
 else
     echo "You are running this script as a non-root user."
 fi
