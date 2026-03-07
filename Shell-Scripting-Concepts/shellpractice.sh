670ebeb9d1366797f52fa400b162565e67a8d8ca
Nikhil <nirhalnihar8344@gmail.com>
2026-03-08 00:39:22 +0530

    latest update

--- Shell-Scripting-Concepts/shellpractice.sh
+++ Shell-Scripting-Concepts/shellpractice.sh
@@ -1,3 +1,15 @@
 
 userid = $(id -u)
 echo "Current User ID: $userid"
+
+if [ $userid -eq 0 ]; then
+    echo "You are running this script as root."
+    echo "You have root privileges."
+    echo " Install Nginx Web Server "
+    apt update
+    apt install nginx -y
+else
+    echo "You are running this script as a non-root user."
+fi
+
+
