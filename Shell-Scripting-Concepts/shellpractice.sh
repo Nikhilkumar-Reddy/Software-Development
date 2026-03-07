362476111f64d879a227e7943ff81af4983d4b77
Nikhil <nirhalnihar8344@gmail.com>
2026-03-08 01:16:43 +0530

    latest update

--- Shell-Scripting-Concepts/shellpractice.sh
+++ Shell-Scripting-Concepts/shellpractice.sh
@@ -6,13 +6,13 @@ if [ $userid -eq 0 ]; then
     echo "You are running this script as root."
     echo "You have root privileges."
     echo " Install Nginx Web Server "
-    dnf install nginxx -y
+    dnf install nginx -y
 else
     echo "You are running this script as a non-root user."
     exit 1
 fi
 
-if [ -f "/etc/nginx/nginx.conf" ]; then
+if [ -f "/etc/nginx/nginx.conf" ]; then   
     echo "Nginx is installed successfully!"
     dnf remove nginx -y
 else
