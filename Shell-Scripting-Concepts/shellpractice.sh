d75d46336fe0944b8fab374f4c251b2be4f4934a
Nikhil <nirhalnihar8344@gmail.com>
2026-03-08 01:18:41 +0530

    latest update

--- Shell-Scripting-Concepts/shellpractice.sh
+++ Shell-Scripting-Concepts/shellpractice.sh
@@ -5,8 +5,8 @@ echo "Current User ID: $userid"
 if [ $userid -eq 0 ]; then
     echo "You are running this script as root."
     echo "You have root privileges."
-    echo " Install Nginx Web Server "
-    dnf install nginx -y
+    echo " Install MySQL Database "
+    dnf install mysql -y
 else
     echo "You are running this script as a non-root user."
     exit 1
@@ -14,7 +14,6 @@ fi
 
 if [ -f "/etc/nginx/nginx.conf" ]; then   
     echo "Nginx is installed successfully!"
-    dnf remove nginx -y
 else
     echo "Nginx installation failed."
 
