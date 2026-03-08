e7e225c939856dc66d74080c27173ebed089e572
Nikhil <nirhalnihar8344@gmail.com>
2026-03-08 21:35:29 +0530

    Shell Scripting Practice

--- Shell-Scripting-Concepts/shellpractice.sh
+++ Shell-Scripting-Concepts/shellpractice.sh
@@ -1,41 +1,31 @@
 #!/bin/bash
-userid=$(id -u)
-echo "Current User ID: $userid"
 
-Server=$1
+USERID=$(id -u)
+echo "current user is: $USERID"
 
-if [ $userid -eq 0 ]; then
+if [ $USERID -eq 0 ]; then
     echo "You are running this script as root."
     echo "You have root privileges."
-    echo " Install $Server server "
-    dnf install $Server -y
 else
     echo "You are running this script as a non-root user."
     exit 1
 fi
 
-if [ $? -eq 0 ]; then   
-    echo "$Server is installed successfully!"
-else
-    echo "$Server installation failed."
-    exit 1
-fi
-
-
-echo " Which server do you want to uninstall? :"
-
-read server_uninstall
-read -p "Enter the 'yes' to uninstall or 'no' to keep: " uninstall_choice
-
-if [ "$uninstall_choice" == "yes" ]; then
-    dnf remove $server_uninstall -y
+Function() {
     if [ $? -eq 0 ]; then   
-        echo "$server_uninstall is uninstalled successfully!"
+       echo "$s1 successfully!"
     else
-        echo "$server_uninstall uninstallation failed."
+        echo "$s1 failed."
         exit 1
-    fi
-else
-    echo "You chose not to uninstall $server_uninstall."
 fi
+
+}
+
+dnf install nginx -y
+Function nginx is installed
+
+
+dnf install mysql -y
+Function mysql is installed
+
 
