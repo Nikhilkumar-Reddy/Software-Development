5f42c752500f167fea2fce00c31bce33d7e0d4f8
Nikhil <nirhalnihar8344@gmail.com>
2026-03-08 22:56:30 +0530

    Shell Scripting Practice

--- Shell-Scripting-Concepts/shellpractice.sh
+++ Shell-Scripting-Concepts/shellpractice.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+
+R="\e[31m"  # Red color code
+G="\e[32m"  # Green color code
+Y="\e[33m"  # Yellow color code
+N="\e[0m"   # Normal color code
+
 USERID=$(id -u)  # Get the current user ID and store it in a variable
 echo "current user is: $USERID"  
 
@@ -19,9 +25,9 @@ fi
 
 Function() {
     if [ $1 -eq 0 ]; then    # Check the exit status of the last command
-       echo " $2 successfully!" | tee -a $LOGS_FILE
+       echo -e "$G $2 successfully!" | tee -a $LOGS_FILE
     else
-        echo " $2 failed !" | tee -a $LOGS_FILE  # used to write the output to both console and log file
+        echo -e "$R $2 failed !" | tee -a $LOGS_FILE  # used to write the output to both console and log file
         exit 1
 fi
 
@@ -32,11 +38,10 @@ fi
 for package in nginx mysql node.js; do
     dnf list installed $package &>> $LOGS_FILE
     if [ $? -eq 0 ]; then
-        echo "$package is already installed." | tee -a $LOGS_FILE
-        exit 1
+        echo -e "$Y $package is already installed." | tee -a $LOGS_FILE 
     else
         dnf install $package -y  &>> $LOGS_FILE | tee -a $LOGS_FILE
-        Function $? "$package is installed now"
+        Function -e $? " $N $package is installed now"
     fi
 done
 
