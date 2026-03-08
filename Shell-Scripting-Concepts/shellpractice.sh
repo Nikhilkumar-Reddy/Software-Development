7ff7b7bb1ab3956a16a61b130b485ddc17c51729
Nikhil <nirhalnihar8344@gmail.com>
2026-03-08 22:19:45 +0530

    Shell Scripting Practice

--- Shell-Scripting-Concepts/shellpractice.sh
+++ Shell-Scripting-Concepts/shellpractice.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
-USERID=$(id -u)
-echo "current user is: $USERID"
+USERID=$(id -u)  # Get the current user ID and store it in a variable
+echo "current user is: $USERID"  
 
-LOGS_FOLDER="/var/log/shell-script"
-LOGS_FILE="$LOGS_FOLDER/$0.log"
-mkdir -p $LOGS_FOLDER
-if [ $USERID -eq 0 ]; then
+LOGS_FOLDER="/var/log/shell-script"  # Define the logs folder path
+LOGS_FILE="$LOGS_FOLDER/$0.log"   # Define the log file path using the script name
+
+# Create the logs folder if it doesn't exist
+mkdir -p $LOGS_FOLDER     
+# Check if the user ID is 0 (root)
+if [ $USERID -eq 0 ]; then   
     echo "You are running this script as root."
     echo "You have root privileges."
 else
@@ -15,20 +18,20 @@ else
 fi
 
 Function() {
-    if [ $? -eq 0 ]; then   
+    if [ $? -eq 0 ]; then    # Check the exit status of the last command
        echo " $1 successfully!"
     else
-        echo " $1 failed."
+        echo " $1 failed." | tee -a $LOGS_FILE  # used to write the output to both console and log file
         exit 1
 fi
 
 }
 
-dnf install nginx -y  &>> $LOGS_FILE
+dnf install nginx -y  &>> $LOGS_FILE | tee -a $LOGS_FILE
 Function "nginx is installed"
 
 
-dnf install mysql -y  &>> $LOGS_FILE
+dnf install mysql -y  &>> $LOGS_FILE | tee -a $LOGS_FILE
 Function "mysql is installed"
 
 
